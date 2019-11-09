48e80b3e6d525731df5f781fd3bbd09d25e4335f
allankof <54761510+allankof@users.noreply.github.com>
2019-11-09 22:31:57 +0800

    3D跳躍練習

--- UnityProject/Assets/Boy.cs
+++ UnityProject/Assets/Boy.cs
@@ -7,6 +7,21 @@ public class Boy : MonoBehaviour
     [Header("跳躍高度"), Range(100, 1500)]
     public int jump = 100;
 
+    private Animator ani;
+
+    private Rigidbody rb;
+    bool isGround;
+
+    public AudioClip soundJump;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void Update()
     {
         Move();
@@ -15,5 +30,29 @@ public class Boy : MonoBehaviour
     private void Move()
     {
         transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "地板")
+        {
+            isGround = true;
+        }
+    }
+
+    public void Jump()
+    {
+        if (isGround)
+        {
+            isGround = false;
+            ani.SetBool("跳躍開關", true);
+            rb.AddForce(new Vector3(0, jump, 0));
+            audioSource.PlayOneShot(soundJump, 0.6f);
+        }
+    }
+
+    public void ResetAnimator()
+    {
+        ani.SetBool("跳躍開關", false);
     }
 }
